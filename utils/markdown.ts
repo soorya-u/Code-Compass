d8347665985e939b64cea52690cc088002efb610
Soorya U <sooryau7@gmail.com>
2024-03-08 11:42:25 +0530

    feat: Added Markdown Contents
    Signed-off-by: Soorya U <sooryau7@gmail.com>

---
+++ utils/markdown.ts
@@ -0,0 +1,112 @@
+import { content } from "@/content";
+
+export const markdown = [
+  {
+    name: "Angular",
+    content: content.angular,
+  },
+  {
+    name: "Bash",
+    content: content.bash,
+  },
+  {
+    name: "CSS",
+    content: content.css,
+  },
+  {
+    name: "Docker",
+    content: content.docker,
+  },
+  {
+    name: "Flask",
+    content: content.flask,
+  },
+  {
+    name: "Git",
+    content: content.git,
+  },
+  {
+    name: "GraphQL",
+    content: content.graphQL,
+  },
+  {
+    name: "HTML",
+    content: content.html,
+  },
+  {
+    name: "Javascript",
+    content: content.javascript,
+  },
+  {
+    name: "LaTeX",
+    content: content.latex,
+  },
+  {
+    name: "Mojo",
+    content: content.mojo,
+  },
+  {
+    name: "MongoDB",
+    content: content.mongoDB,
+  },
+  {
+    name: "NextJS",
+    content: content.nextJS,
+  },
+  {
+    name: "Prisma",
+    content: content.primsa,
+  },
+  {
+    name: "Python",
+    content: content.python,
+  },
+  {
+    name: "React Native",
+    content: content.reactNative,
+  },
+  {
+    name: "React",
+    content: content.react,
+  },
+  {
+    name: "Redis",
+    content: content.redis,
+  },
+  {
+    name: "Rust",
+    content: content.rust,
+  },
+  {
+    name: "SCSS",
+    content: content.scss,
+  },
+  {
+    name: "SQL",
+    content: content.sql,
+  },
+  {
+    name: "Tailwind CSS",
+    content: content.tailwindCSS,
+  },
+  {
+    name: "Tanstack Query",
+    content: content.tanstackQuery,
+  },
+  {
+    name: "Tauri",
+    content: content.tauri,
+  },
+  {
+    name: "Tkinter",
+    content: content.tkinter,
+  },
+  {
+    name: "Typescript",
+    content: content.typescript,
+  },
+  {
+    name: "Zod",
+    content: content.zod,
+  },
+];
