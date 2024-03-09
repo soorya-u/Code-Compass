382bcf54c3454eeffdbc4dbcf8f04a9740918b8c
Soorya U <sooryau7@gmail.com>
2024-03-09 09:17:32 +0530

    feat: Customized Code Highligher
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- components/CodeHighlighter/index.tsx
+++ components/CodeHighlighter/index.tsx
@@ -1,28 +1,59 @@
-import CodeHighlighter from "react-native-code-highlighter";
-import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
 
-function CodeHighligher({
+import RNCodeHighlighter from "react-native-code-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+export default function CodeHighlighter({
   code,
   language,
 }: {
   code: string | string[];
-  language: string;
+  language: string | undefined;
 }) {
+  useFonts({
+    "Jetbrains-Mono-Nerd": require("@/assets/fonts/JetBrainsMonoNerdFont-Medium.ttf"),
+  });
+
   return (
     <>
-      <CodeHighlighter
-        hljsStyle={atomOneDarkReasonable}
+      <RNCodeHighlighter
+        textStyle={styles.text}
+        codeTagProps={{
+          style: styles.codeTag,
+        }}
+        hljsStyle={atomOneDark}
         language={language}
         scrollViewProps={{
-          contentContainerStyle: {
-            paddingHorizontal: 10,
-          },
+          style: styles.scrollView,
+          contentContainerStyle: styles.container,
         }}
       >
         {code}
-      </CodeHighlighter>
+      </RNCodeHighlighter>
     </>
   );
 }
 
-export default CodeHighligher;
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "Jetbrains-Mono-Nerd",
+  },
+  codeTag: {
+    backgroundColor: "#282C34",
+    width: "95%",
+    marginTop: 5,
+    marginBottom: 5,
+    paddingTop: 10,
+    paddingBottom: 10,
+    justifyContent: "center",
+    alignSelf: "center",
+    borderRadius: 5,
+  },
+  container: {
+    paddingHorizontal: 15,
+  },
+  scrollView: {
+    borderRadius: 5,
+  },
+});
