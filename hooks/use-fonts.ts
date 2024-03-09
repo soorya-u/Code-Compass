e83430a2eba50c432dc2653458abcabb08754991
Soorya U <sooryau7@gmail.com>
2024-03-09 21:36:41 +0530

    feat: Added New Font hooks
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- hooks/use-fonts.ts
+++ hooks/use-fonts.ts
@@ -1,16 +1,51 @@
-import { useFonts as useFont, loadAsync } from "expo-font";
+import { useFonts as useFont } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from "react";
 
 export const useJetBrainsNerdFont = () => {
   SplashScreen.preventAutoHideAsync();
+
   const [fontsLoaded, fontError] = useFont({
     "Jetbrains-Mono-Nerd": require("@/assets/fonts/JetBrainsMonoNerdFont-Medium.ttf"),
   });
+
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded || fontError) {
       await SplashScreen.hideAsync();
     }
   }, [fontsLoaded, fontError]);
+
   return { jetBrainsNerd: "Jetbrains-Mono-Nerd", onLayout: onLayoutRootView };
+};
+
+export const usePoppinsFont = () => {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded, fontError] = useFont({
+    Poppins: require("@/assets/fonts/Poppins-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  return { poppins: "Poppins", onLayout: onLayoutRootView };
+};
+
+export const useUbuntuNerdFont = () => {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded, fontError] = useFont({
+    "Ubuntu-Nerd": require("@/assets/fonts/UbuntuNerdFont-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  return { ubuntuNerd: "Ubuntu-Nerd", onLayout: onLayoutRootView };
 };
