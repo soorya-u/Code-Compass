966f903166262044e772926aa21894cb51cad605
Soorya U <sooryau7@gmail.com>
2024-03-31 08:44:43 +0530

    chore: Used Defualt TabBarScreen Options
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,39 +4,23 @@ import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 
 import { useFonts } from "@/hooks/use-fonts";
 import { useTheme } from "@/hooks/use-theme";
+import { useConstantTabOptions } from "@/hooks/use-screen-options";
 
 function RootLayout() {
   const [fontLoaded] = useFonts();
   const { setTheme } = useTheme();
 
-  const backgroundColor = setTheme("rgb(10 10 10)", "rgb(229 231 235)");
-  const foregroundColor = setTheme("rgb(229 231 235)", "rgb(10 10 10)");
+  const { tabContainerStyle, tabOptions } = useConstantTabOptions();
 
   if (!fontLoaded) return null;
   return (
     <>
       <StatusBar style={setTheme("light", "dark")} />
-      <Tabs
-        sceneContainerStyle={{
-          backgroundColor: backgroundColor,
-        }}
-        screenOptions={{
-          tabBarStyle: {
-            backgroundColor: backgroundColor,
-            borderTopColor: setTheme("#33373b", "#a3acb9"),
-          },
-          tabBarActiveTintColor: foregroundColor,
-          tabBarActiveBackgroundColor: backgroundColor,
-          tabBarInactiveBackgroundColor: backgroundColor,
-          headerTintColor: foregroundColor,
-          headerShadowVisible: false,
-        }}
-      >
+      <Tabs sceneContainerStyle={tabContainerStyle} screenOptions={tabOptions}>
         <Tabs.Screen
           name="(stack)"
           options={{
             tabBarLabel: "Home",
-            headerShown: false,
             tabBarIcon: ({ size, color }) => (
               <Ionicons
                 name="file-tray-full-outline"
@@ -50,7 +34,6 @@ function RootLayout() {
           name="about"
           options={{
             tabBarLabel: "About",
-            headerShown: false,
             tabBarIcon: ({ size, color }) => (
               <MaterialIcons name="info" size={size} color={color} />
             ),
@@ -60,8 +43,6 @@ function RootLayout() {
           name="profile"
           options={{
             tabBarLabel: "Profile",
-            headerTitle: "Profile",
-            headerShown: false,
             tabBarIcon: ({ size, color }) => (
               <MaterialIcons name="person" size={size} color={color} />
             ),
