7c90366b3ef6e3d78c9a79191105b3eb4dfac430
Soorya U <sooryau7@gmail.com>
2024-04-03 10:42:24 +0530

    chore: Replaced with Link Component for Typed Route
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- components/MarkdownTitle/index.tsx
+++ components/MarkdownTitle/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View, Image, TouchableOpacity } from "react-native";
-import { useRouter } from "expo-router";
+import { Link } from "expo-router";
 import { useTheme } from "@/hooks/use-theme";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -13,35 +13,38 @@ type ItemProp = {
 
 function MarkdownTitle({ item }: { item: ItemProp }) {
   const { setTheme } = useTheme();
-  const { push } = useRouter();
 
   return (
-    <TouchableOpacity
-      className="bg-neutral-50 dark:bg-[rgb(30_30_30)] rounded-[10px]"
-      // @ts-ignore Experimental Static Links
-      onPress={() => push(`md/${item.link}`)}
+    <Link
+      href={{
+        pathname: "/(stack)/md/[file]",
+        params: { file: item.link },
+      }}
+      asChild
     >
-      <View className="flex-row items-center p-[10] gap-[10]">
-        <Image
-          className="w-[35px] h-[35px]"
-          source={item.img}
-          style={
-            item.canInvert && {
-              tintColor: setTheme("white", "black"),
+      <TouchableOpacity className="bg-neutral-50 dark:bg-[rgb(30_30_30)] rounded-[10px]">
+        <View className="flex-row items-center p-[10] gap-[10]">
+          <Image
+            className="w-[35px] h-[35px]"
+            source={item.img}
+            style={
+              item.canInvert && {
+                tintColor: setTheme("white", "black"),
+              }
             }
-          }
-          alt={item.name}
-        />
-        <Text className="text-[16px] pl-1 flex-1 text-black dark:text-white font-['Poppins']">
-          {item.name}
-        </Text>
-        <Ionicons
-          name="chevron-forward"
-          size={20}
-          color={setTheme("rgb(82 82 82)", "rgb(214 211 209)")}
-        />
-      </View>
-    </TouchableOpacity>
+            alt={item.name}
+          />
+          <Text className="text-[16px] pl-1 flex-1 text-black dark:text-white font-['Poppins']">
+            {item.name}
+          </Text>
+          <Ionicons
+            name="chevron-forward"
+            size={20}
+            color={setTheme("rgb(82 82 82)", "rgb(214 211 209)")}
+          />
+        </View>
+      </TouchableOpacity>
+    </Link>
   );
 }
 
