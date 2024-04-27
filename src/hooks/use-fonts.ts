6e6d0b4c4879aab4e558dd3619b80a11175cc21b
Soorya U <sooryau7@gmail.com>
2024-04-27 19:14:10 +0530

    chore: Fixed useFont Hook
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src/hooks/use-fonts.ts
+++ src/hooks/use-fonts.ts
@@ -3,29 +3,20 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
 export const useFonts = () => {
-  const [jetbrainsMonoNerdLoaded] = useFont({
+  const [fontsLoaded] = useFont({
     "Jetbrains-Mono-Nerd": require("@/assets/fonts/JetBrainsMonoNerdFont-Medium.ttf"),
-  });
-  const [poppinsLoaded] = useFont({
     Poppins: require("@/assets/fonts/Poppins-Medium.ttf"),
-  });
-  const [ubuntuNerdLoaded] = useFont({
     "Ubuntu-Nerd": require("@/assets/fonts/UbuntuNerdFont-Medium.ttf"),
-  });
-  const [inderLoaded] = useFont({
     Inder: require("@/assets/fonts/Inder-Regular.ttf"),
   });
 
   useEffect(() => {
     async function init() {
       await SplashScreen.preventAutoHideAsync();
-      if (jetbrainsMonoNerdLoaded && poppinsLoaded && ubuntuNerdLoaded)
-        await SplashScreen.hideAsync();
+      if (fontsLoaded) await SplashScreen.hideAsync();
     }
     init();
-  }, [jetbrainsMonoNerdLoaded, poppinsLoaded, ubuntuNerdLoaded, inderLoaded]);
+  }, [fontsLoaded]);
 
-  const isLoaded =
-    jetbrainsMonoNerdLoaded && poppinsLoaded && ubuntuNerdLoaded && inderLoaded;
-  return [isLoaded];
+  return [fontsLoaded];
 };
